3970bfb21257e634cf7ea5d05fa2ca967753299d
iamzhz <iamzhz@foxmail.com>
2025-05-02 13:31:42 +0800

    add: github actions

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'dist',
+  base: '/when-i-have-time/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
