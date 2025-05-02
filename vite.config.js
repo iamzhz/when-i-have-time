85808ee2b6fad5557e1e19cd9c5dd7c125dc6e3b
iamzhz <iamzhz@foxmail.com>
2025-05-02 14:57:31 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/when-i-have-time/',
+  base: '/when-i-have-time/dist/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
