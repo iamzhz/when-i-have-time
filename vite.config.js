33083096727eba94b1f1dfbf028b655bd33b36b8
iamzhz <iamzhz@foxmail.com>
2025-05-02 13:22:53 +0800

    add: github actions

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '_site' : 'dist',
+  base: 'dist',
   plugins: [vue()],
   build: {
     outDir: 'dist',
